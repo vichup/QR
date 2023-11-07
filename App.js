ff441efdd3693e1405082646d383c446d906b21c
vichup <vicentemuggeri@gmail.com>
2023-11-07 19:48:09 -0300

    asd

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [tienePermiso, setTienePermiso] = useState(null);
+  const [escaneado, setEscaneado] = useState(false);
+  const [datosEscaneados, setDatosEscaneados] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setTienePermiso(status === 'Permitido');
+    })();
+  }, []);
+
+  const manejarCodigoDeBarrasEscaneado = ({ type, data }) => {
+    setEscaneado(true);
+    setDatosEscaneados(data);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contenedor}>
+      <BarCodeScanner
+        onBarCodeScanned={escaneado ? undefined : manejarCodigoDeBarrasEscaneado}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {escaneado && (
+        <View style={styles.contenedorDatos}>
+          <Text style={styles.textoDatos}>{datosEscaneados}</Text>
+        </View>
+      )}
+      {tienePermiso === null && (
+        <Text>Esperando permiso para acceder a la cámara.</Text>
+      )}
+      {tienePermiso === false && (
+        <Text>No tienes permiso para acceder a la cámara.</Text>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contenedor: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contenedorDatos: {
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    padding: 10,
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+  },
+  textoDatos: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
